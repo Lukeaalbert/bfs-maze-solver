45eb99d06c8d2a0b68af12fd9fdeec97c7dc99da
Luke Albert <128676520+Lukeaalbert@users.noreply.github.com>
2023-10-24 18:07:12 -0700

    Create mazeio.cpp

---
+++ mazeio.cpp
@@ -0,0 +1,57 @@
+/*
+mazeio.cpp
+*/
+
+#include <iostream>
+#include <fstream>
+#include "mazeio.h"
+using namespace std;
+ 
+
+char** read_maze(char* filename, int* rows, int* cols)
+{
+  ifstream ifile(filename); //opening file
+
+  if ( ifile.fail() ) //checking if fail open
+  {
+    return NULL;
+  }
+  
+  ifile >> *rows >> *cols; //reading rows and cols
+
+  if ( ifile.fail () ) //checking if failed
+  {
+    return NULL;
+  }
+
+  char** mazeArray = new char* [*rows]; //creation of mazeArray (on heap)
+
+  for (int i = 0; i < *rows; i++)
+  {
+    mazeArray[i] = new char[*cols];
+  }
+
+  for (int i = 0; i < *rows; i++)
+  {
+    for (int j = 0; j < *cols; j++)
+    {
+      ifile >> mazeArray[i][j]; 
+    }
+  }
+
+  return mazeArray; //returning mazeArray
+}
+
+//print maze function
+void print_maze(char** maze, int rows, int cols)
+{
+  cout << rows << ' ' << cols << endl; //printing rows and cols
+  for (int i = 0; i < rows; i++) //printing maze
+  {
+    for (int j = 0; j < cols; j++)
+    {
+      cout << maze[i][j];
+    }
+    cout << endl;
+  }
+}
