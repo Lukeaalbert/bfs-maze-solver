763eb684cb957b8a514a10b2ed3793d5781ef64c
Luke Albert <128676520+Lukeaalbert@users.noreply.github.com>
2023-10-24 18:08:05 -0700

    Create queue.cpp

---
+++ queue.cpp
@@ -0,0 +1,36 @@
+/*
+queue.cpp
+*/
+
+#include "queue.h"
+
+//Constructor function. Creating tail, head, and contents.
+Queue::Queue(int maxlen) {
+    head = 0;
+    tail = 0;
+    contents = new Location[maxlen];
+}
+
+//Destructor. releases resources. C++ will call it automatically.
+Queue::~Queue() {
+    delete [] contents;
+}
+
+//Insert a new Location at the end/back of our list
+void Queue::add_to_back(Location loc) {
+    contents[tail] = loc;
+    tail++;
+}
+
+//Return and "remove" the oldest Location not already extracted
+Location Queue::remove_from_front() {
+    int returnHead = head;
+    head++;
+    return contents[returnHead];
+}
+
+//Is this Queue empty? (did we extract everything added?)
+//This is complete, you don't need to change it.
+bool Queue::is_empty() {
+    return head == tail;
+}
